62bf4c99be6342a18863c5a34e14f803acfbe742
JanNDen <jc@pntr.eu>
2021-02-20 18:17:29 +0200

    Finished convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,52 +1,83 @@
 /*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the handler logic below
+ *
+ *
+ */
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    return this.splitNumberAndUnit(input)
+      .initNumber.split("/")
+      .reduce((acc, num) => acc / this.numberChecker(num));
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    return this.splitNumberAndUnit(input).initUnit;
   };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+  this.getReturnUnit = function (initUnit) {
+    const units = {
+      liquids: ["gal", "L"],
+      weights: ["lbs", "Kg"],
+      distances: ["mi", "Km"],
+    };
+    for (type in units) {
+      if (units[type].indexOf(initUnit) >= 0) {
+        const switchedIndex = 1 - units[type].indexOf(initUnit);
+        return units[type][switchedIndex];
+      }
+    }
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.spellOutUnit = function (unit) {
+    const unitSpelling = {
+      gal: "gallons",
+      L: "litres",
+      lbs: "pounds",
+      Kg: "kilograms",
+      mi: "miles",
+      Km: "kilometers",
+    };
+    return unitSpelling[unit];
   };
-  
+
+  this.convert = function (initNum, initUnit) {
+    const unitCaltulations = {
+      gal: 3.78541,
+      L: 1 / 3.78541,
+      lbs: 0.453592,
+      Kg: 1 / 0.453592,
+      mi: 1.60934,
+      Km: 1 / 1.60934,
+    };
+    const calculation = initNum * unitCaltulations[initUnit];
+    return parseFloat(calculation).toFixed(5);
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    const words = [
+      initNum,
+      this.spellOutUnit(initUnit),
+      "converts to",
+      returnNum,
+      this.spellOutUnit(returnUnit),
+    ];
+    return words.join(" ");
+  };
+
+  this.splitNumberAndUnit = function (input) {
+    // Helper function that returns an object with separated number and unit
+    return input.match(/(?<initNumber>[.0-9\/]*)(?<initUnit>[a-zA-Z]*)/).groups;
+  };
+
+  this.numberChecker = function (input) {
+    // Check whether we have a real number
+    // Especially important here to check the number of dots
+    if (isNaN(input)) return undefined;
+    else return input;
+  };
 }
 
 module.exports = ConvertHandler;
